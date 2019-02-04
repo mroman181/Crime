dc4cc9af457694542ce716ef803dc92ea0c4fb67
Marc <mroman181@iesfbmoll.org>
2019-02-04 17:07:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dam2a21/crime/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.example.dam2a21.crime;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+
+
+
+}
